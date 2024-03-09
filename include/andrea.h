a23b75ae2a7734fe9c9b9e2b64fd5f793dfcf05f
Mateusz Karcz <mateusz.karcz@interia.eu>
2024-03-09 22:53:03 +0100

    repo: extract common DOS definitions header

--- include/andrea.h
+++ include/andrea.h
@@ -1,17 +1,9 @@
 #ifndef _ANDREA_H_
 #define _ANDREA_H_
 
-#include <assert.h>
 #include <i86.h>
-#include <stdint.h>
 
-#ifndef far
-#ifdef EDITING
-#define far
-#else // EDITING
-#define far __far
-#endif // EDITING
-#endif // far
+#include <dosdef.h>
 
 typedef unsigned far (*andrea_registration_callback)(uint16_t far *);
 typedef uint16_t andrea_module;
@@ -30,37 +22,6 @@ typedef enum
     ANDREA_ERROR_TOO_MANY_MODULES = 0x82,
     ANDREA_ERROR_NO_EXPORTS = 0x83,
 } andrea_status;
-
-typedef enum
-{
-    STDOUT = 1,
-    STDERR = 2,
-} dos_stream;
-
-#pragma pack(push, 1)
-typedef struct
-{
-    char      int20h[2];
-    uint16_t  seg_tail;
-    uint8_t   _reserved1;
-    char      dispatch[5];
-    void far *intv_terminate;
-    void far *intv_break;
-    void far *intv_error;
-    char      _reserved2[22];
-    uint16_t  seg_environment;
-    char      _reserved3[34];
-    char      int21h_retf[3];
-    char      _reserved4[9];
-    char      fcb1[16];
-    char      fcb2[20];
-    uint8_t   cmdline_len;
-    char      cmdline[127];
-} dos_psp;
-#pragma pack(pop)
-
-static_assert(256 == sizeof(dos_psp),
-              "DOS PSP size doesn't match specification");
 
 extern andrea_module
 andrea_load(const char *name);
