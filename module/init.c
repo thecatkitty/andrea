8c76169b4a155548f3c8c39ee4b25d8da7bb2a20
Mateusz Karcz <mateusz.karcz@interia.eu>
2024-03-09 14:47:20 +0100

    module: add a common log API

--- module/init.c
+++ module/init.c
@@ -1,4 +1,3 @@
-#include <dos.h>
 #include <stddef.h>
 
 #include <andrea.h>
@@ -7,19 +6,6 @@ int __errno;
 
 extern void far
 andrea_exptabl(void);
-
-size_t
-strlen(const char *str)
-{
-    size_t count = 0;
-
-    while (*str++)
-    {
-        count++;
-    }
-
-    return count;
-}
 
 static void far *
 _deserialize_pointer(const char far *buffer)
@@ -35,40 +21,14 @@ _deserialize_pointer(const char far *buffer)
     return (void far *)intptr;
 }
 
-static char *
-_utoa10(unsigned value, char *buffer)
-{
-    int digits = 9999 < value  ? 5
-                 : 999 < value ? 4
-                 : 99 < value  ? 3
-                 : 9 < value   ? 2
-                               : 1;
-
-    for (int i = 1; i <= digits; i++)
-    {
-        buffer[digits - i] = '0' + (value % 10);
-        value /= 10;
-    }
-
-    buffer[digits] = 0;
-    return buffer;
-}
-
-static void
-_print(dos_stream stream, const char *str)
-{
-    unsigned bytes;
-    _dos_write(stream, _CV_FP(str), strlen(str), &bytes);
-}
-
 uint8_t
 module_init(dos_psp far *psp)
 {
-    _print(STDOUT, "Module entry.\r\n");
+    LOG("entry");
 
     if (8 >= psp->cmdline_len)
     {
-        _print(STDERR, "Module exit, missing parameter.\r\n");
+        LOG("exit, missing parameter");
         return ANDREA_ERROR_MISSING_PARAMETER;
     }
 
@@ -76,7 +36,7 @@ module_init(dos_psp far *psp)
     {
         if (('@' > psp->cmdline[i]) || ('O' < psp->cmdline[i]))
         {
-            _print(STDERR, "Module exit, invalid parameter.\r\n");
+            LOG("exit, invalid parameter");
             return ANDREA_ERROR_INVALID_PARAMETER;
         }
     }
@@ -85,13 +45,26 @@ module_init(dos_psp far *psp)
         (andrea_registration_callback)_deserialize_pointer(psp->cmdline + 1);
 
     unsigned status = callback((uint16_t far *)andrea_exptabl);
-    _print(STDOUT, "Callback returned ");
 
-    char buffer[32];
-    _utoa10(status, buffer);
-    _print(STDOUT, buffer);
-    _print(STDOUT, ".\r\n");
+    LOG("exit, status:");
+    switch (status)
+    {
+    case ANDREA_SUCCESS:
+        LOG("  success");
+        break;
 
-    _print(STDOUT, "Module exit.\r\n");
+    case ANDREA_ERROR_TOO_MANY_MODULES:
+        LOG("  too many modules!");
+        break;
+
+    case ANDREA_ERROR_NO_EXPORTS:
+        LOG("  no exports!");
+        break;
+
+    default:
+        LOG("  unknown error!");
+        break;
+    }
+
     return status;
 }
