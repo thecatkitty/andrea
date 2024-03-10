f5605290b3c548006d49083a9b68f5556c68311b
Mateusz Karcz <mateusz.karcz@interia.eu>
2024-03-10 20:22:11 +0100

    host: retrieve procedure by name

--- examples/chost.c
+++ examples/chost.c
@@ -22,22 +22,23 @@ _try_load_module(const char *name)
 }
 
 static void far *
-_try_get_procedure(andrea_module module, uint16_t ordinal)
+_try_get_procedure(andrea_module module, const char far *name)
 {
-    void far *procedure = andrea_get_procedure(module, ordinal);
+    void far *procedure = andrea_get_procedure(module, name);
     if (0 == procedure)
     {
-        fprintf(stderr, "Cannot get procedure %u of %04X!\n", ordinal, module);
+        fprintf(stderr, "Cannot get procedure %04X:%04X of %04X!\n",
+                FP_SEG(name), FP_OFF(name), module);
         goto end;
     }
 
-    printf("Got procedure %u: %04X:%04X", ordinal, FP_SEG(procedure),
-           FP_OFF(procedure));
+    printf("Got procedure %04X:%04X: %04X:%04X", FP_SEG(name), FP_OFF(name),
+           FP_SEG(procedure), FP_OFF(procedure));
 
-    char name[32];
-    if (andrea_get_procedure_name(procedure, name, sizeof(name)))
+    char proc_name[32];
+    if (andrea_get_procedure_name(procedure, proc_name, sizeof(proc_name)))
     {
-        printf(" (%s)", name);
+        printf(" (%s)", proc_name);
     }
     puts("");
 
@@ -72,14 +73,15 @@ main(int argc, char *argv[])
     TRY_STEP(module2, _try_load_module("module2.exe"));
 
     lpfnsquare square;
-    TRY_STEP(square, (lpfnsquare)_try_get_procedure(module1, 1));
+    TRY_STEP(square,
+             (lpfnsquare)_try_get_procedure(module1, ANDREA_ORDINAL(1)));
 
     STEP("Call square", printf("42 squared is %d\n", square(42)));
 
     STEP("Free module1", andrea_free(module1));
 
     lpfnhello hello;
-    TRY_STEP(hello, (lpfnhello)_try_get_procedure(module2, 1));
+    TRY_STEP(hello, (lpfnhello)_try_get_procedure(module2, "hello"));
 
     STEP("Call hello", hello());
 
