62aae2e0db27759c0fdc8335afb6ddb8f3b48939
Mateusz Karcz <mateusz.karcz@interia.eu>
2024-03-11 22:44:24 +0100

    cpp: enable calling of far procedures

--- examples/cpphost.cpp
+++ examples/cpphost.cpp
@@ -1,22 +1,32 @@
 #include <cstdio>
 
 #include <andrea.hpp>
+#include <andrea/farproc.hpp>
 
 int
 main(int argc, char *argv[])
 {
-    auto module = andrea_module::open("module2.exe");
-    if (!module.has_value())
+    auto module1 = andrea_module::open("module1.exe");
+    if (!module1.has_value())
     {
-        std::fputs("Cannot open the module!\n", stderr);
+        std::fputs("Cannot open the module1!\n", stderr);
         return 1;
     }
 
-    uint32_t procedure = module->get_procedure(1);
-    std::printf("Procedure pointer is %08lX.\n", procedure);
+    auto module2 = andrea_module::open("module2.exe");
+    if (!module2.has_value())
+    {
+        std::fputs("Cannot open the module2!\n", stderr);
+        return 1;
+    }
 
-    procedure = module->get_procedure("hello");
-    std::printf("Procedure pointer is %08lX.\n", procedure);
+    andrea_farproc<unsigned(unsigned)> square{module1->get_procedure("square")};
+    std::printf("Procedure pointer is %08lX.\n", square.get());
+    std::printf("Square of 42 is %u\n", square(42));
+
+    andrea_farproc<unsigned()> hello{module2->get_procedure("hello")};
+    std::printf("Procedure pointer is %08lX.\n", hello.get());
+    hello();
 
     return 0;
 }
