afe99d68c64b414eb5bf7ecb7048f056c3edabc2
Mateusz Karcz <mateusz.karcz@interia.eu>
2024-03-19 07:55:43 +0100

    module: allow multiple extern of the same import

--- examples/module2.c
+++ examples/module2.c
@@ -4,7 +4,7 @@ __attribute__((section(".text"))) static const char far _hello[] =
     "Hello World!\r\n$";
 
 ANDREA_IMPORT(dos_putfs);
-#define dos_putfs ANDREA_IMPORT_FPTR(void, dos_putfs, (const char far *))
+#define dos_putfs ANDREA_EXTERN(void, dos_putfs, (const char far *))
 
 void far
 hello(void)
