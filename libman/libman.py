5824f53693bce79229d4e30663479c5038339ed2
Mateusz Karcz <mateusz.karcz@interia.eu>
2024-03-27 20:17:52 +0100

    libman: allow file output

--- libman/libman.py
+++ libman/libman.py
@@ -1,24 +1,45 @@
-import argparse
+from argparse import ArgumentParser
+from io import IOBase
+from sys import stdout
 
 from andrea.module import Module
 
-parser = argparse.ArgumentParser()
-parser.add_argument("module", help="name of the module file")
 
+TEXT_FORMATS = ["text"]
+BINARY_FORMATS = []
+FORMATS = TEXT_FORMATS + BINARY_FORMATS
+
+
+parser = ArgumentParser()
+parser.add_argument("module", help="name of the module file")
+parser.add_argument("-o", dest="output",
+                    help="name of the output file", default="--")
+parser.add_argument("-f", dest="format",
+                    help="format of the output data", choices=FORMATS)
 args = parser.parse_args()
 
+format = args.format if args.format is not None else "text"
+output = stdout if args.output == "--" else open(
+    args.output, "w" if format in TEXT_FORMATS else "wb")
 module = Module.from_file(args.module)
 
-print(f"Container image format {module.format}")
-print(f"Andrea header at {module.header_offset:04X}, size {module.header.size}")
-print()
 
-print("Exports:" if len(module.exports) else "No exports")
-for i, exp in enumerate(module.exports):
-    print(f"  {i}: {(module.cs_base + exp[1]):04X} {exp[0]}")
-print()
+def to_text(module: Module, output: IOBase):
+    print(f"Container image format {module.format}", file=output)
+    print(
+        f"Andrea header at {module.header_offset:04X}, size {module.header.size}", file=output)
+    print(file=output)
 
-print("Imports:" if len(module.imports) else "No imports")
-for i, imp in enumerate(module.imports):
-    print(f"  {i}: {imp}")
-print()
+    print("Exports:" if len(module.exports) else "No exports", file=output)
+    for i, exp in enumerate(module.exports):
+        print(f"  {i}: {(module.cs_base + exp[1]):04X} {exp[0]}", file=output)
+    print(file=output)
+
+    print("Imports:" if len(module.imports) else "No imports", file=output)
+    for i, imp in enumerate(module.imports):
+        print(f"  {i}: {imp}", file=output)
+    print(file=output)
+
+
+if format == "text":
+    to_text(module, output)
