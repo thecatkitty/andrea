42e029ce4da1d27109d778a9fdee4ad072e6d58a
Mateusz Karcz <mateusz.karcz@interia.eu>
2024-03-27 20:41:19 +0100

    libman: add assembler output format

--- libman/libman.py
+++ libman/libman.py
@@ -1,11 +1,12 @@
 from argparse import ArgumentParser
 from io import IOBase
+from os.path import basename
 from sys import stdout
 
 from andrea.module import Module
 
 
-TEXT_FORMATS = ["text"]
+TEXT_FORMATS = ["text", "asm"]
 BINARY_FORMATS = []
 FORMATS = TEXT_FORMATS + BINARY_FORMATS
 
@@ -18,7 +19,8 @@ parser.add_argument("-f", dest="format",
                     help="format of the output data", choices=FORMATS)
 args = parser.parse_args()
 
-format = args.format if args.format is not None else "text"
+format = args.format if args.format is not None else "asm" if basename(
+    args.output).endswith((".asm", ".s", ".S")) else "text"
 output = stdout if args.output == "--" else open(
     args.output, "w" if format in TEXT_FORMATS else "wb")
 module = Module.from_file(args.module)
@@ -41,5 +43,33 @@ def to_text(module: Module, output: IOBase):
     print(file=output)
 
 
+def to_asm(module: Module, output: IOBase):
+    print(".code16", file=output)
+    print(".intel_syntax noprefix", file=output)
+    print(file=output)
+
+    for name, _ in module.exports:
+        print(f".global {name}", file=output)
+        print(f"{name}: ljmp [cs:__imp_{name}]", file=output)
+    print(file=output)
+
+    print(".section .andrea.imports", file=output)
+    for name, _ in module.exports:
+        print(f"__imp_{name}:", file=output)
+        print(f".global __imp_{name}", file=output)
+        print(f"__imp_{name}:", file=output)
+        print(f".word 0", file=output)
+        print(f".word __impstr_{name}", file=output)
+    print(file=output)
+
+    print(".section .andrea.impstrs", file=output)
+    for name, _ in module.exports:
+        print(f"__impstr_{name}: .asciz \"{name}\"", file=output)
+    print(file=output)
+
+
 if format == "text":
     to_text(module, output)
+
+elif format == "asm":
+    to_asm(module, output)
