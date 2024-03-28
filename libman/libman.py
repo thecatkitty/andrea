aa951c35aaba7b330bbc378e9edfe53f1be78a61
Mateusz Karcz <mateusz.karcz@interia.eu>
2024-03-28 11:47:36 +0100

    libman: add library output format

--- libman/libman.py
+++ libman/libman.py
@@ -1,13 +1,16 @@
+import os
+import subprocess
+
 from argparse import ArgumentParser
 from io import IOBase
-from os.path import basename
+from tempfile import TemporaryDirectory
 from sys import stdout
 
 from andrea import AssemblyGenerator, Module
 
 
 TEXT_FORMATS = ["text", "asm"]
-BINARY_FORMATS = []
+BINARY_FORMATS = ["lib"]
 FORMATS = TEXT_FORMATS + BINARY_FORMATS
 
 
@@ -19,8 +22,9 @@ parser.add_argument("-f", dest="format",
                     help="format of the output data", choices=FORMATS)
 args = parser.parse_args()
 
-format = args.format if args.format is not None else "asm" if basename(
-    args.output).endswith((".asm", ".s", ".S")) else "text"
+base = os.path.basename(args.output)
+format = args.format if args.format is not None else "asm" if base.endswith(
+    (".asm", ".s", ".S")) else "lib" if base.endswith(".a") else "text"
 output = stdout if args.output == "--" else open(
     args.output, "w" if format in TEXT_FORMATS else "wb")
 module = Module.from_file(args.module)
@@ -59,8 +63,41 @@ def to_asm(module: Module, output: IOBase):
         print(chunk, file=output)
 
 
+def to_lib(module: Module, output: str):
+    generator = AssemblyGenerator(module)
+    tmpdir = TemporaryDirectory()
+    files = list(f"{tmpdir.name}/{name}.o" for name, _ in module.exports)
+
+    for file, text, imports, impstrs in zip(files, generator.emit_text(), generator.emit_imports(), generator.emit_impstrs()):
+        gas = subprocess.Popen(
+            ["ia16-elf-as", "-o", file], stdin=subprocess.PIPE)
+        gas.stdin.writelines(line.encode() for line in [
+            generator.emit_prolog(),
+            text,
+            "\n.section .andrea.imports\n",
+            imports,
+            "\n.section .andrea.impstrs\n",
+            impstrs,
+            "\n"
+        ])
+        gas.stdin.close()
+        if 0 != gas.wait():
+            raise subprocess.CalledProcessError(gas.returncode, gas.args)
+
+    subprocess.check_call(["ia16-elf-ar", "-rcs", output, *files])
+    tmpdir.cleanup()
+
+
 if format == "text":
     to_text(module, output)
 
 elif format == "asm":
     to_asm(module, output)
+
+elif format == "lib":
+    if output == stdout:
+        raise ValueError("Cannot write a library to STDOUT")
+
+    output.close()
+    os.remove(args.output)
+    to_lib(module, args.output)
